b6caebd649bcf8b258ea93fc07c0756ec7b6cd7f
AmirDamavandi <amirromanista2004@gmail.com>
2024-09-15 14:15:35 +0330

    created model User

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,92 @@
 from django.contrib import admin
+from django import forms
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.core.exceptions import ValidationError
+from .models import User
 
 # Register your models here.
+
+
+class UserCreationForm(forms.ModelForm):
+    """A form for creating new users. Includes all the required
+    fields, plus a repeated password."""
+
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label="Password confirmation", widget=forms.PasswordInput
+    )
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'username', 'email']
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise ValidationError('Passwords don\'t match')
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+        if commit:
+            user.save()
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+    """A form for updating users. Includes all the fields on
+    the user, but replaces the password field with admin's
+    disabled password hash display field.
+    """
+
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = User
+        fields = ['email', 'password', 'date_of_birth', 'is_active', 'is_admin']
+
+
+class UserAdmin(BaseUserAdmin):
+    # The forms to add and change user instances
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference specific fields on auth.User.
+    list_display = [
+        'first_name', 'last_name', 'username', 'email', 'date_joined',
+        'phone_number', 'is_verified', 'gender', 'is_suspended', 'is_admin'
+    ]
+    list_filter = ['is_admin', 'is_verified', 'is_suspended', 'date_joined', 'date_of_birth']
+    fieldsets = [
+        (None, {'fields': ['email', 'username', 'password']}),
+        ('Personal info', {'fields': [
+            'first_name', 'last_name', 'phone_number', 'location', 'website',
+            'gender', 'bio', 'account_type',
+        ]}),
+        ('Verify', {'fields': ['is_verified']}),
+        ('Permissions', {'fields': ['is_admin']}),
+    ]
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = [
+        (
+            None,
+            {
+                'classes': ['wide'],
+                'fields': ['first_name', 'username', 'email', 'password1', 'password2'],
+            },
+        ),
+    ]
+    search_fields = ['email', 'username', 'first_name']
+    ordering = ['email']
+    filter_horizontal = []
+
+
+admin.site.register(User, UserAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,69 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+from .manager import MyUserManager
+from django.core.exceptions import ValidationError
+
 
 # Create your models here.
+
+class User(AbstractBaseUser):
+    first_name = models.CharField(max_length=30, help_text='enter your first name')
+    last_name = models.CharField(max_length=30, help_text='enter your last name', blank=True, null=True)
+    header = models.ImageField(upload_to='users_header/', null=True, blank=True)
+    avatar = models.ImageField(upload_to='users_avatar/', null=True, blank=True)
+    username = models.CharField(max_length=30, unique=True, help_text='enter your username')
+    email = models.EmailField(unique=True, help_text='enter your email address', blank=True, null=True)
+    phone_number = models.CharField(
+        max_length=11, unique=True, help_text='enter your phone number', blank=True, null=True
+    )
+    is_verified = models.BooleanField(default=False)
+    account_types = (('public', 'Public'), ('private', 'Private'))
+    account_type = models.CharField(max_length=8, choices=account_types, default='public')
+    date_of_birth = models.DateField(blank=True, null=True)
+    bio = models.TextField(max_length=300, null=True, blank=True)
+    location = models.CharField(max_length=50, null=True, blank=True)
+    website = models.URLField(null=True, blank=True)
+    genders = (('male', 'Male'), ('female', 'Female'), ('prefer not to say', 'Prefer not to say'))
+    gender = models.CharField(max_length=17, choices=genders)
+    is_suspended = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    objects = MyUserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['first_name', 'email']
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+    def __str__(self):
+        return self.username
+
+    def clean(self):
+        if not self.email and not self.phone_number:
+            raise ValidationError("Either email or phone number must be provided.")
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
+
+    def has_perm(self, perm, obj=None):
+        """Does the user have a specific permission?"""
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        """Does the user have permissions to view the app `app_label`?"""
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        """Is the user a member of staff?"""
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
+
